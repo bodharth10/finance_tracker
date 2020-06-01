b43d53846a4350c23afe1e07009949cc641f6707
Bodharth Lonkar <lbodharth@yahoo.in>
2020-06-01 23:37:02 +0400

    changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :user_stocks
-	get 'search_stock', to: 'stocks#search'
+  get 'search_stock', to: 'stocks#search'
   get 'users/my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   devise_for :users
